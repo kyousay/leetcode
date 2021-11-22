d0d7e4c14f250b76d820fb26ccb8a57e5cc30423
kyousay <yusei5884@gmail.com>
2021-11-22 21:57:39 +0900

    2021/11/22

--- 1876. Substrings of Size Three with Distinct Characters.js
+++ 1876. Substrings of Size Three with Distinct Characters.js
@@ -21,3 +21,18 @@
 // Memory Usage: 40.5 MB, less than 59.61% of JavaScript online submissions for Substrings of Size Three with Distinct Characters.
 
 // URL: https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+
+// another answer
+var countGoodSubstrings = function(s) {
+    let count = 0;
+    for(let i = 0; i < s.length - 2; ++i) {
+		// compare the three values, they shouldn't be equal
+        if(s[i] !== s[i + 1] && s[i] !== s[i + 2] && s[i + 1] !== s[i + 2]) {
+            ++count;
+        }
+    }
+    return count;
+}
+
+// Runtime: 75 ms, faster than 85.99% of JavaScript online submissions for Substrings of Size Three with Distinct Characters.
+// Memory Usage: 39.2 MB, less than 94.79% of JavaScript online submissions for Substrings of Size Three with Distinct Characters.
