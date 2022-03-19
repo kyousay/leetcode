be68c6920416bfbf9dc527a4d50423a9e6ac6fa3
kyousay <yusei5884@gmail.com>
2022-03-19 12:30:21 +0900

    2022/3/19

--- JS/Easy/1413. Minimum Value to Get Positive Step by Step Sum.js
+++ JS/Easy/1413. Minimum Value to Get Positive Step by Step Sum.js
@@ -20,4 +20,46 @@
 // Runtime: 95 ms, faster than 32.70% of JavaScript online submissions for Minimum Value to Get Positive Step by Step Sum.
 // Memory Usage: 42.2 MB, less than 33.33% of JavaScript online submissions for Minimum Value to Get Positive Step by Step Sum.
 
-// another answer
+// another answer1
+// numsの中で一番低くなる瞬間の値を取ってこれれば、そこに１足した値にすれば、O(n)にできる
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var minStartValue = function(nums) {
+    let minSum = +Infinity;
+    nums.reduce((a, i) => {
+        let res = a + i;
+        minSum = Math.min(minSum, res)
+        return res;
+    }, 0);
+    console.log(minSum);
+    let result = 1 - minSum;
+    return result > 0 ? result : 1;
+};
+
+// Runtime: 87 ms, faster than 51.83% of JavaScript online submissions for Minimum Value to Get Positive Step by Step Sum.
+// Memory Usage: 42.2 MB, less than 32.93% of JavaScript online submissions for Minimum Value to Get Positive Step by Step Sum.
+
+// another answer2
+// こちらはmaxの値を
+var minStartValue = function(nums) {
+    let num = Math.min(...nums)
+    if(num > 0){
+        return 1
+    }
+    
+    let max = 1
+    let result = max
+    for(let i = 0; i < nums.length; i++){   
+        max += nums[i] 
+        if(max <= 0){ 
+            // if max = -5, Add (-1 * 5) + 1 to result
+            result = result + (-1 *max) + 1
+            max =  1 
+        }
+    }
+    return result
+};
+// Runtime: 76 ms, faster than 68.90% of JavaScript online submissions for Minimum Value to Get Positive Step by Step Sum.
+// Memory Usage: 42 MB, less than 42.68% of JavaScript online submissions for Minimum Value to Get Positive Step by Step Sum.
---
+++ JS/Easy/509. Fibonacci Number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var fib = function(n) {
+    if(n === 0 || n === 1) return n;
+    return fib(n - 1) + fib(n - 2);
+};
+
+// URL: https://leetcode.com/problems/fibonacci-number/
+
+// Runtime: 84 ms, faster than 62.32% of JavaScript online submissions for Fibonacci Number.
+// Memory Usage: 41.8 MB, less than 56.06% of JavaScript online submissions for Fibonacci Number.
+
+// フィボナッチ数列とは
+// イタリアの数学者フィボナッチ(1170~1259年頃)が紹介した数列を「フィボナッチ数列」と言います。 １、１、２、３、５、８、１３、２１、３４、５５、８９、１４４、２３３、３７７… 「どの数字も前２つの数字を足した数字」という規則の数列です。
+
+// another answer
