c1dd80c54e8a8558b22849f7854ec6bb41c38aa0
kyousay <yusei5884@gmail.com>
2022-03-22 10:58:07 +0900

    2022/3/22

---
+++ JS/InCompleted/897. Increasing Order Search Tree.js
@@ -0,0 +1,26 @@
+// getValsまでのロジックは考え付いていた。
+
+var increasingBST = function(root) {
+	const arr = []
+
+	function getVals(node){
+		if(!node) return
+		getVals(node.left)
+		arr.push(node.val)
+		getVals(node.right)
+	}
+
+	getVals(root)
+
+	const tree = new TreeNode()
+	let curr = tree
+	for(let i=0; i< arr.length; i++){
+		curr.right = new TreeNode(arr[i]);
+		curr = curr.right
+	}
+
+	return tree.right
+};
+
+// Runtime: 88 ms, faster than 47.35% of JavaScript online submissions for Increasing Order Search Tree.
+// Memory Usage: 42.9 MB, less than 47.35% of JavaScript online submissions for Increasing Order Search Tree.
