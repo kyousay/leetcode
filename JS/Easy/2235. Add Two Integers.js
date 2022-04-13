8bb960aafc0f97ada04aa32fddbcb3adb9b9ec5f
kyousay <yusei5884@gmail.com>
2022-04-13 17:06:32 +0900

    2022/4/13

---
+++ JS/Easy/2235. Add Two Integers.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+ const sum = (num1, num2) => num1 + num2;
+
+// URL:  https://leetcode.com/problems/add-two-integers/
+
+// Runtime: 60 ms, faster than 87.50% of JavaScript online submissions for Add Two Integers.
+// Memory Usage: 42.3 MB, less than 25.00% of JavaScript online submissions for Add Two Integers.
